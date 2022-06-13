5a7fd38360d79aa65146a7f4512001deb41ab64c
Bogdan0109 <102528317+Bogdan0109@users.noreply.github.com>
2022-06-13 18:46:59 +0300

    about

---
+++ src/js/dropdown.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openDropdownBtn: document.querySelector('[data-dropdown-open]'),
+      closeDropdownBtn: document.querySelector('[data-dropdown-close]'),
+      dropdown: document.querySelector('[data-dropdown]'),
+    };
+  
+    refs.openDropdownBtn.addEventListener('click', toggleDropdown);
+    refs.closeDropdownBtn.addEventListener('click', toggleDropdown);
+  
+    function toggleDropdown() {
+      refs.dropdown.classList.toggle('is-hidden');
+      refs.dropdown.classList.toggle('visually-hidden');
+    }
+  })();
